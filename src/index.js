8643963576f0584ae9aae80b063d211e62431826
Manuel Iriart <iriartmanuel81@gmail.com>
2022-10-26 21:10:06 -0300

    desafio

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 const express = require('express');
-const mainRouter = require('./routes/index');
-
-/** INICIALIZACION API con EXPRESS */
 const app = express();
+const path = require('path');
+const mainRouter = require('./routes/index');
+const { ProductosController } = require('./controller/products')
+const viewsFolderPath = path.resolve(__dirname, '../views');
+
 const puerto = 8080;
 const server = app.listen(puerto, () =>
   console.log('Server en puerto', puerto)
 );
 
-server.on('error', (err) => {
-  console.log('ERROR ATAJADO', err);
-});
-
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/**
- * DEFINICION DE LOS ROUTERS
- */
+app.set('views', viewsFolderPath);
+app.set('view engine', 'pug');
 
-app.use('/', mainRouter);
+app.get('/', (req, res) => {
+
+    res.render('formulario');
+    console.log(productos)
+
+})
+
+app.use('/api', mainRouter);
+
+app.use((err, req, res, next) => {
+
+    const status = err.status || 500;
+    const message = err.message || 500;
+
+    res.status(status).json({ message });
+})
+
+module.exports = app;
